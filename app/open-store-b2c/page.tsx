d420dd292c7c74fc0eefde27e5aaa0148b504731
Travex Fix <fix@travex.app>
2026-06-19 13:32:55 +0000

    fix: settings subscription functions (selectPeriod/selectPlan/upgradeNow), open-store-b2b/b2c route fixes

--- app/open-store-b2c/page.tsx
+++ app/open-store-b2c/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect } from 'react'
 export default function OpenStorePage() {
-  useEffect(() => { window.location.replace('/dashboard/open-store.html') }, [])
+  useEffect(() => { window.location.replace('/dashboard/open-store-b2c.html') }, [])
   return null
 }
